a433c29c5f0de50e21a6ca20d61a03a08661efaf
x8522207x <x8522207x@gmail.com>
2024-12-30 23:43:42 +0800

    try enhance video download speed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
 let startY = 0;
-(function (d) {
-    var config = {
-        kitId: 'gof2byk',
-        scriptTimeout: 3000,
-        async: true
-    },
-        h = d.documentElement, t = setTimeout(function () { h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"; }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a; h.className += " wf-loading"; tk.src = 'https://use.typekit.net/' + config.kitId + '.js'; tk.async = true; tk.onload = tk.onreadystatechange = function () { a = this.readyState; if (f || a && a != "complete" && a != "loaded") return; f = true; clearTimeout(t); try { Typekit.load(config) } catch (e) { } }; s.parentNode.insertBefore(tk, s)
-})(document);
 
 $(document).ready(() => {
     $('.modal_wrap').css('display', 'none');
@@ -502,7 +494,7 @@ const openVideo = (video, path) => {
             allowfullscreen></iframe>`);
     } else if (path) {
         $('.modal_source').append(
-            `<video class="modal_youtube youtube--2" loop autoplay playsinline controls controlslist="nodownload" preload="metadata"><source src=${path}></video>`);
+            `<video class="modal_youtube youtube--2" loop autoplay playsinline controls controlslist="nodownload" preload="metadata"><source src=${path} type="video/mp4"></video>`);
     }
 };
 
